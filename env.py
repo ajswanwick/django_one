bc852fe40510e7ea8c7858f7bf1aa89abde5e111
ajswanwick <a.j.swanwick@gmail.com>
2025-01-21 14:16:02 +0000

    initial commit

---
+++ env.py
@@ -0,0 +1,4 @@
+import os 
+
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:8DwEak3viCHT@ep-nameless-lake-a2pagrka.eu-central-1.aws.neon.tech/doll_jumbo_plug_188831"
+os.environ["SECRET_KEY"] = "eo_@x5@z1p1(ec-10r)y3#^ghrl-h3--a7zqslecow9k7il*c-"
