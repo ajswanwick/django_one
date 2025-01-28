25a24025ccde696fe4a61bdff13f2de15e0bc4ee
ajswanwick <a.j.swanwick@gmail.com>
2025-01-28 13:56:04 +0000

    fix site

---
+++ env.py
@@ -0,0 +1,5 @@
+import os 
+
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:8DwEak3viCHT@ep-nameless-lake-a2pagrka.eu-central-1.aws.neon.tech/shun_crept_boney_151787"
+os.environ["SECRET_KEY"] = "eo_@x5@z1p1(ec-10r)y3#^ghrl-h3--a7zqslecow9k7il*c-"
+os.environ["CLOUDINARY_URL"] = "cloudinary://644232834486932:dAxN090rQEa4oRozL7gO7DzyeCQ@dsg0ziqng"
